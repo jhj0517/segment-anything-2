0b6d050c5ffca3aacc27cf16bc45b250c728e68d
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-08-16 21:59:43 +0900

    Fix typo

--- sam2/sam2_image_predictor.py
+++ sam2/sam2_image_predictor.py
@@ -398,7 +398,7 @@ class SAM2ImagePredictor:
             # boxes are added at the beginning) to sam_prompt_encoder
             if concat_points is not None:
                 if box_coords.size(0) > 1 or box_labels.size(0) > 1:
-                    warning.warn("If you use a combination of box and point as a prompt, only a single "
+                    warnings.warn("If you use a combination of box and point as a prompt, only a single "
                                  "combination is supported. Automatically uses only the first combination.")
                     concat_points = (concat_points[0][:, :1, :], concat_points[1][:, :1])
                     box_labels = box_labels[:1]
